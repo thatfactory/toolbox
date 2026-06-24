b5d2af87730a8dd345f9ea551990c3da32034778
Fernando Fernandes <fernando.fernandes@blacklane.com>
2026-06-24 11:18:08 +0200

    Lower minimum platform versions to 26

--- Package.swift
+++ Package.swift
@@ -5,8 +5,8 @@ import PackageDescription
 let package = Package(
     name: "Toolbox",
     platforms: [
-        .iOS(.v27),
-        .macOS(.v27)
+        .iOS(.v26),
+        .macOS(.v26)
     ],
     products: [
         .library(
